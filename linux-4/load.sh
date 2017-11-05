d5ed7908631135efe04f23bcb43fb81989918b01
xio4 <xio4you@gmail.com>
2017-11-06 03:24:36 +0500

    chore(compile): remove xz archiving

--- linux-4/load.sh
+++ linux-4/load.sh
@@ -1,4 +1,4 @@
-insmod snd-soc-nau8824.ko.xz
-insmod snd-soc-sst-cht-bsw-nau8824.ko.xz
+insmod snd-soc-nau8824.ko
+insmod snd-soc-sst-cht-bsw-nau8824.ko
 rmmod snd_intel_sst_acpi
-insmod snd-intel-sst-acpi.ko.xz
+insmod snd-intel-sst-acpi.ko
